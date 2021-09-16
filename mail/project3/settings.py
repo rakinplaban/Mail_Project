4a1917b07fdd3d14097f6ca394d5e0e36ba3bd43
rakinplaban <rakinshahriar54@gmail.com>
2021-09-16 16:41:06 +0600

    successfully connected to pgsql

--- mail/project3/settings.py
+++ mail/project3/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'HOST': 'localhost',
+        'HOST': '127.0.0.1',
         'PORT': '5432',
         'PASSWORD': 'qnr63363'
     }
